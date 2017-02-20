658cf8dac24beb4672c6841888e37e4a11f99418
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:53:22 +0100

    Modificacion del cuarto ejercicio

--- ej4.py
+++ ej4.py
@@ -14,3 +14,13 @@ for hoteles in data["resources"]:
             print "El hotel",hoteles["dc:title"]," no tiene url"
         elif hoteles["lpgc:web"] != "":
             print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
+    if hoteles["dc:title"][+4:].upper()==nombre:
+        if hoteles["lpgc:web"] == "":
+            print "El hotel",hoteles["dc:title"]," no tiene url"
+        elif hoteles["lpgc:web"] != "":
+            print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
+    if hoteles["dc:title"][+3:].upper()==nombre:
+        if hoteles["lpgc:web"] == "":
+            print "El hotel",hoteles["dc:title"]," no tiene url"
+        elif hoteles["lpgc:web"] != "":
+            print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
