40f1959f21551465c966b8277027be9b8392b6ab
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:52:02 +0100

    Modificacion del cuarto ejercicio

--- ej4.py
+++ ej4.py
@@ -5,3 +5,7 @@ with open ('hoteles.json') as data_file:
 nombre=raw_input("Introduce el nombre del hotel: ").upper()
 for hoteles in data["resources"]:
     if hoteles["dc:title"][+6:].upper()==nombre:
+        if hoteles["lpgc:web"] == "":
+            print "El hotel",hoteles["dc:title"]," no tiene url"
+        else:
+            print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
