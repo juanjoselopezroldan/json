32e93b4e41e4807d48f83b25b191bfbef0a5da73
Juan Jose <jjlr1994@gmail.com>
2017-02-20 20:01:41 +0100

    Modificacion del primer ejercicio

--- ej1.py
+++ ej1.py
@@ -7,3 +7,14 @@ e2=0
 e3=0
 e4=0
 e5=0
+for hoteles in data["resources"]:
+    if hoteles["dc:title"].count("*")==5:
+        e5=e5+1
+    elif hoteles["dc:title"].count("*")==4:
+        e4=e4+1
+    elif hoteles["dc:title"].count("*")==3:
+        e3=e3+1
+    elif hoteles["dc:title"].count("*")==2:
+        e2=e2+1
+    elif hoteles["dc:title"].count("*")==1:
+        e1=e1+1
