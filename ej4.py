1d2597cc97bdfc50239b13caeab4e01369b3556a
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:54:45 +0100

    Modificacion del cuarto ejercicio

--- ej4.py
+++ ej4.py
@@ -3,11 +3,17 @@ import json
 with open ('hoteles.json') as data_file:
     data=json.load(data_file)
 nombre=raw_input("Introduce el nombre del hotel: ").upper()
+
 for hoteles in data["resources"]:
-    if hoteles["dc:title"][+5:].upper()==nombre:
+    if hoteles["dc:title"][+6:].upper()==nombre:
         if hoteles["lpgc:web"] == "":
             print "El hotel",hoteles["dc:title"]," no tiene url"
         else:
+            print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
+    if hoteles["dc:title"][+5:].upper()==nombre:
+        if hoteles["lpgc:web"] == "":
+            print "El hotel",hoteles["dc:title"]," no tiene url"
+        elif hoteles["lpgc:web"] != "":
             print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
     if hoteles["dc:title"][+4:].upper()==nombre:
         if hoteles["lpgc:web"] == "":
@@ -20,11 +26,6 @@ for hoteles in data["resources"]:
         elif hoteles["lpgc:web"] != "":
             print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
     if hoteles["dc:title"][+2:].upper()==nombre:
-        if hoteles["lpgc:web"] == "":
-            print "El hotel",hoteles["dc:title"]," no tiene url"
-        elif hoteles["lpgc:web"] != "":
-            print "El hotel",hoteles["dc:title"]," tiene la siguiente url: ",hoteles["lpgc:web"]
-    if hoteles["dc:title"][+1:].upper()==nombre:
         if hoteles["lpgc:web"] == "":
             print "El hotel",hoteles["dc:title"]," no tiene url"
         elif hoteles["lpgc:web"] != "":
