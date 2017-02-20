4acf2ab46a2dcd4e3bbbf8c34e0221157f21fc5b
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:58:54 +0100

    Modificacion del quinto ejercicio

--- ej5.py
+++ ej5.py
@@ -3,3 +3,9 @@ import json
 with open ('hoteles.json') as data_file:
     data=json.load(data_file)
 cod=(raw_input("Introduce el nombre del hotel: "))
+
+for hoteles in data["resources"]:
+    if hoteles["dc:identifier"]==cod:
+        print hoteles["dc:title"]
+        print hoteles["dc:description"]
+        print "Localizacion: lat ",hoteles["geo:lat"]," y log ",hoteles["geo:long"]
