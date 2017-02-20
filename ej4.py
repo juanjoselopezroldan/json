5ac343db9147d7dba6cdb1529ca31c34ccf997a7
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:51:13 +0100

    Modificacion del cuarto ejercicio

--- ej4.py
+++ ej4.py
@@ -3,3 +3,5 @@ import json
 with open ('hoteles.json') as data_file:
     data=json.load(data_file)
 nombre=raw_input("Introduce el nombre del hotel: ").upper()
+for hoteles in data["resources"]:
+    if hoteles["dc:title"][+6:].upper()==nombre:
