850cc91da0f4d574e6a07008af53b0071b8ced5a
Juan Jose <jjlr1994@gmail.com>
2017-02-20 22:21:24 +0100

    Modificacion del tercer ejercicio

--- ej3.py
+++ ej3.py
@@ -6,3 +6,13 @@ with open ('hoteles.json') as data_file:
 estrellas=int(raw_input("Busqueda de hoteles por estrellas: "))
 
     for hoteles in data["resources"]:
+        if hoteles["dc:title"].count("*")==estrellas:
+            print hoteles["dc:title"]
+        elif hoteles["dc:title"].count("*")==estrellas:
+            print hoteles["dc:title"]
+        elif hoteles["dc:title"].count("*")==estrellas:
+            print hoteles["dc:title"]
+        elif hoteles["dc:title"].count("*")==estrellas:
+            print hoteles["dc:title"]
+        elif hoteles["dc:title"].count("*")==estrellas:
+            print hoteles["dc:title"]
