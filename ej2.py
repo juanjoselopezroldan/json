50582baa8092ab9c1f4268cf9e21a9998e01c3d8
Juan Jose <jjlr1994@gmail.com>
2017-02-20 20:05:43 +0100

    Modificacion del segundo ejercicio

--- ej2.py
+++ ej2.py
@@ -12,3 +12,14 @@ for hoteles in data["resources"]:
         medio.append(hoteles["dc:title"])
     if hoteles["lpgc:precio"]=="Alto" or hoteles["lpgc:precio"]=="alto":
         alto.append(hoteles["dc:title"])
+print "Hoteles con precio ECONOMICO"
+for ec in economico:
+    print ec
+
+print "Hoteles con precio MEDIO"
+for me in medio:
+    print me
+
+print "Hoteles con precio ALTO"
+for al in alto:
+    print al
