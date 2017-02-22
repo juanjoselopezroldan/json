6fa1d24be887b041a0f1402eb7acec06b061ca72
juan jose lopez <jjlr1994@gmail.com>
2017-02-22 12:38:48 +0100

    Mod ejercicio

--- ej1.py
+++ ej1.py
@@ -1,29 +1,29 @@
 # -*- coding: utf-8 -*-
-#Lista la cantidad de Hoteles clasificados por estrellas
+#Lista los hoteles por precio ("Alto","Medio","Economico")
 
 import json
 with open ('hoteles.json') as data_file:
     data=json.load(data_file)
-e1=0
-e2=0
-e3=0
-e4=0
-e5=0
+alto=[]
+medio=[]
+economico=[]
 for hoteles in data["resources"]:
-    if hoteles["dc:title"].count("*")==5:
-        e5=e5+1
-    elif hoteles["dc:title"].count("*")==4:
-        e4=e4+1
-    elif hoteles["dc:title"].count("*")==3:
-        e3=e3+1
-    elif hoteles["dc:title"].count("*")==2:
-        e2=e2+1
-    elif hoteles["dc:title"].count("*")==1:
-        e1=e1+1
+    if hoteles["lpgc:precio"]=="Economico":
+        economico.append(hoteles["dc:title"])
+    if hoteles["lpgc:precio"]=="medio" or hoteles["lpgc:precio"]=="Precio medio":
+        medio.append(hoteles["dc:title"])
+    if hoteles["lpgc:precio"]=="Alto" or hoteles["lpgc:precio"]=="alto":
+        alto.append(hoteles["dc:title"])
 
+print "Hoteles con precio ECONOMICO"
+for ec in economico:
+    print ec
 
-print "Hay",e5,"Hoteles de 5 estrellas"
-print "Hay",e4,"Hoteles de 4 estrellas"
-print "Hay",e3,"Hoteles de 3 estrellas"
-print "Hay",e2,"Hoteles de 2 estrellas"
-print "Hay",e1,"Hoteles de 1 estrella"
+print "Hoteles con precio MEDIO"
+for me in medio:
+    print me
+
+print "Hoteles con precio ALTO"
+for al in alto:
+    print al
+
