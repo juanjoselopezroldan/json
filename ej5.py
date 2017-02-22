09611db89e8b71b0933e7018e25d6de703a24411
juan jose lopez <jjlr1994@gmail.com>
2017-02-22 12:37:30 +0100

    Retocado del ultimo ejercicio

--- ej5.py
+++ ej5.py
@@ -1,18 +1,19 @@
 # -*- coding: utf-8 -*-
-#Introduce un codigo de hotel y que muestre el nombre del hotel, la localizacion y una descripcion del mismo
+#Introduce los dos ultimos numeros de un hotel y que muestre el nombre del hotel, la localizacion y una descripcion del mismo
 
 import json
 with open ('hoteles.json') as data_file:
     data=json.load(data_file)
 
-cod=(raw_input("Introduce el nombre del hotel: "))
+tel=raw_input("Introduce los dos ultimos digitos del numero: ")
 
+no="false"
 
 for hoteles in data["resources"]:
-    if hoteles["dc:identifier"]==cod:
+    if hoteles["lpgc:telefono"][-2:]==tel:
         print hoteles["dc:title"]
         print hoteles["dc:description"]
         print "Localizacion: lat ",hoteles["geo:lat"]," y log ",hoteles["geo:long"]
-        exit()
-if hoteles["dc:identifier"]!=cod:
-        print "Error, codigo no encontrado"
+        no="true"
+if no=="false":
+        print "Error, hotel no encontrado"
